30e6ce220083ca6fb3a4a790579db990290240e7
Ranjeet Harishchandre <ranjeetharishchandre@gmail.com>
2020-12-26 13:09:56 +0530

    first commit at 1.22.24

---
+++ src/Home.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Home.css";
+import Product from "./Product";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/31/prime/Gateway/2020/May/gaming_1500x600._CB431281464_.jpg"
+          alt=""
+        />
+        <div className="home__row">
+          <Product />
+          <Product />
+        </div>
+        <div className="home__row">
+          <Product />
+          <Product />
+          <Product />
+        </div>
+
+        <div className="home__row">
+          <Product />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDwQsMkCbgZAtSc0l4uH2ySZORKdbt7Ffc",
+  authDomain: "clone-84ea5.firebaseapp.com",
+  projectId: "clone-84ea5",
+  storageBucket: "clone-84ea5.appspot.com",
+  messagingSenderId: "77153946794",
+  appId: "1:77153946794:web:26b6a6d5c3a1d4438cd4d3",
+  measurementId: "G-NLVNZ0G5JY",
+};
